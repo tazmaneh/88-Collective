956f492cc3734fd9dbfdfd287f9a00172f407e6d
tazmaneh <tazmanehoon75@gmail.com>
2017-10-04 12:16:59 +0200

    mock

---
+++ 88-Mock/Temp-site/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { componentFactoryName } from '@angular/compiler';
+import { Path } from '@angular-devkit/core/src';
+import { patch } from 'webdriver-js-extender/built/lib';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { WelcomeComponent } from "./components/welcome/welcome.component";
+
+
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: "welcome", component: WelcomeComponent },
+  {path: "", redirectTo: "/carousel",pathMatch:"full"},
+  {path:"carousel", component: CarouselComponent}
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
