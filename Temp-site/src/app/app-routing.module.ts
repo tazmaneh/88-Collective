b69976a86005a1bfc6de853b74721b155819009b
Athandile Tembile <athanTembile@outlook.com>
2017-10-05 09:03:41 +0200

    Carousel Update

--- Temp-site/src/app/app-routing.module.ts
+++ Temp-site/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/welcome", pathMatch: "full" },
-  { path: "welcome", component: WelcomeComponent }
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent }
 ];
 
 @NgModule({
